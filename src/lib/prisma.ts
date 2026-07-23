1acd3660ca5bc171a6073b874eb2bdf2605a5543
Pr3v3r <11pravar.singh11@gmail.com>
2026-07-23 14:33:22 +0530

    prisma

--- src/lib/prisma.ts
+++ src/lib/prisma.ts
@@ -1,8 +1,8 @@
-import { PrismaClient } from "@prisma/client";
+import { PrismaClient } from ".prisma/client/default";
 import { PrismaPg } from "@prisma/adapter-pg";
 
-const adapter = new PrismaPg({ 
-  connectionString: process.env.DATABASE_URL! 
+const adapter = new PrismaPg({
+  connectionString: process.env.DATABASE_URL!,
 });
 
 const globalForPrisma = globalThis as unknown as {
@@ -15,6 +15,3 @@ export const prisma =
 if (process.env.NODE_ENV !== "production") {
   globalForPrisma.prisma = prisma;
 }
-
-export type { PrismaClient };
-
